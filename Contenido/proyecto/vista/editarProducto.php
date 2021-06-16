3dd214d23c51452bc489ed22cbf97ffc4ffd2bea
Victor Manuel Anta Castro <anta.castro.victor.manuel@iescamas.com>
2021-06-17 00:00:25 +0200

    *Modificaciones en los css boostrap

--- Contenido/proyecto/vista/editarProducto.php
+++ Contenido/proyecto/vista/editarProducto.php
@@ -25,7 +25,8 @@ $fila = $resultado->fetch_assoc();
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="../css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="../css/cabecera.css" rel="stylesheet">
     <link href="../css/pie.css" rel="stylesheet">
--- Contenido/proyecto/vista/verProductos.php
+++ Contenido/proyecto/vista/verProductos.php
@@ -24,7 +24,7 @@ $resultado = $conn->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="../css/jquery.dataTables.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../css/cabecera.css">
     <link rel="stylesheet" href="../css/pie.css">
